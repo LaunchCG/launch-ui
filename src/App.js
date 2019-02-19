6ef4b84ec051c72b3a3afd04d47740c22668ce98
Alex Miller <amiller@launchcg.com>
2019-02-19 12:39:18 -0800

    initialize react project replace react assets with launch assets

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import launchLogo from './images/launch-logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={launchLogo} className="App-logo" alt="logo" />
+          <p>
+            A Boilerplate Launch UI React Component Library based on Material UI
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
